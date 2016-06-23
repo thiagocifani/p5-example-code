a3b73be01b2e82778ba572b40e7e26ed70685197
thiagocifani <cifani.thiago@gmail.com>
2016-06-23 14:23:43 -0300

    WebGl simple example.

---
+++ 004/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+}
+
+function draw() {
+  background(255);
+  translate(-width/2,-height/2,0); //moves our drawing origin to the top left corner
+  box();
+}
